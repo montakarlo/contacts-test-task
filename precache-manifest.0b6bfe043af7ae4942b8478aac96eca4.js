6811647d7a25a0266c676ca7c65694b78174354c
Dmytro Potapenko <1lindern1@gmail.com>
2020-09-23 07:51:07 +0300

    Updates

---
+++ precache-manifest.0b6bfe043af7ae4942b8478aac96eca4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d2b227f38b23172ccdadbe584c92dc",
+    "url": "/contacts-test-task/index.html"
+  },
+  {
+    "revision": "c61507c4b0aaa3d8f3b2",
+    "url": "/contacts-test-task/static/css/2.08c04568.chunk.css"
+  },
+  {
+    "revision": "29ba36e5b9ca4dfa6e98",
+    "url": "/contacts-test-task/static/css/main.b9ba3ef8.chunk.css"
+  },
+  {
+    "revision": "c61507c4b0aaa3d8f3b2",
+    "url": "/contacts-test-task/static/js/2.d4260af8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contacts-test-task/static/js/2.d4260af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29ba36e5b9ca4dfa6e98",
+    "url": "/contacts-test-task/static/js/main.d686c587.chunk.js"
+  },
+  {
+    "revision": "f3052684746271d02f82",
+    "url": "/contacts-test-task/static/js/runtime-main.3f4ae144.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/contacts-test-task/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/contacts-test-task/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/contacts-test-task/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
